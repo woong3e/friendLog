ee537ef30d21e19505050381ed2e560f15b96181
GUNWOONG JANG <gunwoong32@gmail.com>
2025-05-01 21:15:02 +0900

    modify router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import About from './pages/About';
-import Register from './pages/Register';
 import NotFoundPage from './pages/404';
 import Detail from './pages/Detail';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import PostRegister from './pages/PostRegister';
 
 export default function Router() {
   return (
@@ -13,7 +13,7 @@ export default function Router() {
       <Route path="/" element={<Main />} />
       <Route path="/about" element={<About />} />
       <Route path="/detail/:id" element={<Detail />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/register" element={<PostRegister />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signUp" element={<SignUp />} />
       <Route path="*" element={<NotFoundPage />} />
