36d6abd920ede56d1aa49a43b3c4d3643c7837b3
GUNWOONG JANG <gunwoong32@gmail.com>
2025-05-08 19:47:02 +0900

    style: 스타일 수정

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,4 @@
 const Card = ({ posts }) => {
-  console.log(posts[0], posts[1], posts[2]);
-
   return (
     <div className="bg-white border border-gray-400 shadow-sm md:w-1/4 rounded-xl dark:bg-gray-800 dark:border-gray-700">
       <a href="/detail/1">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="m-4 bg-white rounded-lg shadow-sm dark:bg-gray-900">
-      <div className="w-full max-w-screen-xl p-4 mx-auto md:py-8"></div>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <footer className="flex flex-col justify-end m-1 h-14 mx-1 md:h-14 bg-white rounded-lg shadow-sm dark:bg-gray-900">
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2025{' '}
-        <a href="/" className="hover:underline">
-          wondang
+        <a href="https://github.com/woong3e/friendLog" className="">
+          friendlog
         </a>
-        . friendlog
+        <a href="https://github.com/woong3e" className="ml-1">
+          @woong3e
+        </a>
       </span>
     </footer>
   );
